2bbff947d42919d3b5d71f648b872e5ad47bed0a
Juan Soria <juanzito.857@gmail.com>
2024-03-08 21:01:44 -0300

    Finished

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,15 @@
 import dotenv from 'dotenv';
-dotenv.config();
+dotenv.config()
 import express from 'express';
-import authRoutes from './routes/authRoutes';
+import authRoutes from './routes/authRoutes'
+import usersRoutes from './routes/userRoutes'
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
 
-//Routes
+// Routes
 app.use('/auth', authRoutes)
-
-//User
-
+app.use('/users', usersRoutes)
 
 export default app
